90bf2462ded36e866daee36406658d879abb1e61
Adam Bursey <abursey@gmail.com>
2026-05-03 18:18:50 -0400

    chore: update deployment credentials, configure allowed hosts, add documentation, and remove obsolete environment example

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,10 @@ SERVICE_NAME="transaction-analyzer"
 ALLOWED_EMAILS="abursey@gmail.com,annb1015@gmail.com"
 
 # TODO: Please fill in your secrets before running this script!
-GOOGLE_CLIENT_ID="y464729360910-lth6branimqo13kmirv5cl4vbvsj7tk1.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET="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"
+GOOGLE_CLIENT_ID="489644758279-10834amjc0k3moguamitr22msih1qu59.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET="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"
 SHEET_URL="https://docs.google.com/spreadsheets/d/1BcpK8KVnEHMQdBXSeV2tdcU7_5ch0fthAmbzGteXX-Y/edit?gid=2121452751#gid=2121452751"
+GEMINI_API_KEY="AQ.Ab8RN6LDY98LySzNkVds6E47FSZ4slFP1qNoVDCTZuOOPL5WXQ"
 
 echo "Deploying to Google Cloud Run ($PROJECT_ID)..."
 
@@ -21,6 +22,6 @@ gcloud run deploy $SERVICE_NAME \
   --region $REGION \
   --project $PROJECT_ID \
   --allow-unauthenticated \
-  --set-env-vars="^|^ALLOWED_EMAILS=$ALLOWED_EMAILS|GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID|GOOGLE_CLIENT_SECRET=$GOOGLE_CLIENT_SECRET|SHEET_URL=$SHEET_URL"
+  --set-env-vars="^|^ALLOWED_EMAILS=$ALLOWED_EMAILS|GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID|GOOGLE_CLIENT_SECRET=$GOOGLE_CLIENT_SECRET|SHEET_URL=$SHEET_URL|GEMINI_API_KEY=$GEMINI_API_KEY"
 
 echo "Deployment finished."
---
+++ docs/notes.md
@@ -0,0 +1,1 @@
+[Deployment URL](https://transaction-analyzer-489644758279.us-central1.run.app)
